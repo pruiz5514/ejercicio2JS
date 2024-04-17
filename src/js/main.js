a12065ed3398b7d8a82da4e0d32c5b53adb2a7c7
pruiz5514 <pablo_ruiza@hotmail.com>
2024-04-17 07:45:43 -0700

    Se hace hasta el ejercicio 6

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+// 1
+const num1 = 2;
+const num2 = 4;
+console.log(num1+num2);
+
+// 2
+const simpleString1 = 'Hola';
+const simpleString2 = 'Mundo';
+console.log(simpleString1, simpleString2);
+
+// 3
+const stringUsingString1 = String("JavaScript");
+const stringUsingString2 = String("Ejercicio")
+console.log(stringUsingString1, stringUsingString2);
+
+// 4
+const stringUsingNewString1 = new String("Concatenar");
+const stringUsingNewString2 = new String("Strings");
+console.log(stringUsingNewString1, stringUsingNewString2);
+
+// 5
+console.log(simpleString1.indexOf("l"))
+
+// 6
+console.log(simpleString2.includes("ndo"))
